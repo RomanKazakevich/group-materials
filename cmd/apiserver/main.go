37d85e7c3eedbd80ba066ec6c5c9afd3ac111b55
Роман Казакевич <rkazakevich@MacBook-Air-Roman.local>
2020-02-24 15:42:27 +0300

    feat: add some files

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/RomanKazakevich/group-materials/internal/app/apiserver"
 )
 
 func main() {
-	// s := api.New()
+	s := apiserver.New()
+	if err := s.Start(); err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println("Start main")
 }
